156345afea9febd2b4e512511a4cc105e0bd9f9d
Administrator <Administrator@>
2021-01-16 12:30:06 +0000

    add new columne test1

--- HR/Tables/COUNTRIES.sql
+++ HR/Tables/COUNTRIES.sql
@@ -2,6 +2,7 @@ CREATE TABLE hr.countries (
   country_id CHAR(2 BYTE) NOT NULL CONSTRAINT country_id_nn CHECK ("COUNTRY_ID" IS NOT NULL),
   country_name VARCHAR2(40 BYTE),
   region_id NUMBER,
+  test1 VARCHAR2(20 BYTE),
   CONSTRAINT countr_reg_fk FOREIGN KEY (region_id) REFERENCES hr.regions (region_id),
   CONSTRAINT country_c_id_pk PRIMARY KEY (country_id)
 )
